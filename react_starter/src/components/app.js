15bdf94990538015e5b1b5269c007da08e22b513
jauxsh <joshua.jih.chang@gmail.com>
2017-12-19 18:26:51 -0800

    added react_starter, and modal component

---
+++ react_starter/src/components/app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../assets/css/app.css';
+import logo from '../assets/images/logo.svg';
+import Modal from './modal';
+
+const App = () => (
+    <div>
+        <div className="app">
+            <img src={logo} className="logo rotate"/>
+            <h1>Welcome to React</h1>
+        </div>
+        <Modal />
+    </div>
+);
+
+export default App;
