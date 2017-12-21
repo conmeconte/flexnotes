66f3ebbff6d5b03ce0943aad83d4d0a74addd12a
rslibed <rslibed@gmail.com>
2017-12-20 18:18:58 -0800

    imported panel component

--- react_starter/src/components/app.js
+++ react_starter/src/components/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../assets/css/app.css';
 import logo from '../assets/images/logo.svg';
-// import Panel from './panel/panel';
+import Panel from './panel/panel';
 // import Video from './video/video';
 
 import Modal from './modal';
@@ -12,7 +12,8 @@ const App = () => (
             <img src={logo} className="logo rotate"/>
             <h1>Welcome to React</h1>
         </div>
-        <Modal />
+        {/*<Modal />*/}
+        <Panel/>
     </div>
 );
 
