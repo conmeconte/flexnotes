10258a4ad4ba5560cb62395e99d91c6d751cf2a7
rslibed <rslibed@gmail.com>
2017-12-20 17:39:58 -0800

    Added imports of videos and panel to app js file

--- react_starter/src/components/app.js
+++ react_starter/src/components/app.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import '../assets/css/app.css';
 import logo from '../assets/images/logo.svg';
+// import Panel from './panel/panel';
+// import Video from './video/video';
+
 import Modal from './modal';
 
 const App = () => (
@@ -9,6 +12,7 @@ const App = () => (
             <img src={logo} className="logo rotate"/>
             <h1>Welcome to React</h1>
         </div>
+
         <Modal />
     </div>
 );
