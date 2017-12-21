dee6ca8d0e8310e0970b20c3e0ba857325465c31
rslibed <rslibed@gmail.com>
2017-12-20 17:40:58 -0800

    Added imports of video and panel files to top of app js file

--- react_starter/src/components/app.js
+++ react_starter/src/components/app.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import '../assets/css/app.css';
 import logo from '../assets/images/logo.svg';
+// import Panel from './panel/panel';
+// import Video from './video/video';
+
 import Modal from './modal';
 
 const App = () => (
