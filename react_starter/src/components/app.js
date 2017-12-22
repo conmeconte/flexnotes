51a545877314bc5a8272c733c9a8ca4acfbf527e
brucean52 <bruce.an52@gmail.com>
2017-12-22 11:28:53 -0800

    navbar

--- react_starter/src/components/app.js
+++ react_starter/src/components/app.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import '../assets/css/app.css';
 import logo from '../assets/images/logo.svg';
+
+import Nav from './navbar/nav';
 import Modal from './modal';
 
 const App = () => (
     <div>
         <div className="app">
+            <Nav/>
             <img src={logo} className="logo rotate"/>
             <h1>Welcome to React</h1>
         </div>
