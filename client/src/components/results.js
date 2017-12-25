7062b131c2419fe3d20e61d339b1ddb0bca236c3
rslibed <rslibed@gmail.com>
2017-12-25 14:53:13 -0800

    Created object to start video data

--- client/src/components/results.js
+++ client/src/components/results.js
@@ -1,6 +1,14 @@
 import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const videoData = {
+    vidDimensions: {
+        width: "",
+        height: ""
+    },
+    items: []
+};
+
 class Results extends Component {
     constructor (props) {
         super(props);
@@ -15,6 +23,10 @@ class Results extends Component {
         document.querySelector(".video-container").innerText = "";
         document.querySelector(".video-container").append(iframe);
     }
+    addToPlayList (obj) {
+        videoData.items.push(obj);
+        console.log(videoData);
+    }
     render() {
         const {results} = this.props;
         console.log("Here is the results from the results comp: ", results);
@@ -23,7 +35,7 @@ class Results extends Component {
             return (
                 <div className="result-item col-xs-12" key={index}>
                     <li className="video-items">{item.videoTitle}</li>
-                    <button className="btn-sm btn-success pull-right" onClick={ () => { console.log("Add button was clicked.")} }><span className="glyphicon glyphicon-plus"></span></button>
+                    <button className="btn-sm btn-success pull-right" onClick={ () => { this.addToPlayList(item) } }><span className="glyphicon glyphicon-plus"></span></button>
                     <button className="btn-sm btn-primary pull-right" onClick={ () => { this.play(item.url) }}><span className="glyphicon glyphicon-play"></span></button>
                 </div>
             );
