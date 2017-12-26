e5a8446b8220d9d66f8882c189b694a356b0c455
brucean52 <bruce.an52@gmail.com>
2017-12-25 23:55:27 -0800

    navbar works but I commented it out

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import Panel from './panel';
+import NavBar from '../components/navbar/nav';
 import '../assets/css/app.css';
 
 class App extends Component {
     render() {
         return (
             <div>
+                {/* <NavBar/> */}
                 <Panel/>
             </div>
         );
