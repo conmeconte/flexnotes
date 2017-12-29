c6217307b9c8d4a13b0914350113ec1bc14071f9
brucean52 <bruce.an52@gmail.com>
2017-12-29 14:32:50 -0800

    uncommented out panel

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -8,7 +8,7 @@ class App extends Component {
         return (
             <div>
                 <NavBar/>
-                {/* <Panel/> */}
+                <Panel/>
             </div>
         );
     }
