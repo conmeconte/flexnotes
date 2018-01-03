50afcf7d331e29b596e6365ef35ba1af4850ade2
conmeconte <saja523@hotmail.com>
2018-01-03 12:33:28 -0800

    breaking like a king

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,20 +1,31 @@
 import React, {Component} from 'react';
+import { BrowserRouter, Route} from 'react-router-dom';
+import { connect} from 'react-redux';
+import * as actions from "../actions";
 import Panel from './panel';
 import NavBar from '../components/navbar/nav';
 import Login from './login';
 import '../assets/css/app.css';
 
 class App extends Component {
+    componentDidMount(){
+        this.props.fetchUser();
+    }
+
     render() {
         return (
             <div>
-                <NavBar/>
-                <Panel/>
-
-                <Login />
+               <BrowserRouter >
+                <div className="container">
+                    <Route exact path="/main" component={Panel} />
+                        {/* // <NavBar/>
+                        // <Panel/>
+                        // <Login /> */}
+                </div>
+                </BrowserRouter>
             </div>
         );
     }
 }
 
-export default App;
+export default connect(null, actions)(App);
--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-function Login(){
-    return (
-        <div>
-            <div className="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
-        </div>
-    )
+class Login extends Component{
+    renderContent(){
+        switch (this.props.auth){
+            case null:
+                return;
+            case false:
+                return (
+                    <li><a href="/auth/google" className="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></a></li>
+                );
+            default:
+                return <li><a href="/api/logout">Logout</a></li>;
+        }
+    }
+
+    render() {
+        console.log(this.props);
+        return(
+        <nav>
+            <div>
+                <ul>
+                    {this.renderContent()}
+                </ul>
+            </div>
+        </nav>
+        
+        );
+    }
 }
-
-export default Login;
+function mapStateToProps({ auth }){
+    return {auth};
+}
+export default connect(mapStateToProps)(Login);
