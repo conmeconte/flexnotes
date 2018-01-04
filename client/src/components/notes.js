c15fc62a3f24043502fdf8a03e67911097f4da82
dazcha76 <dazcha@hotmail.com>
2018-01-04 14:56:22 -0800

    added rich text toolbar

--- client/src/components/notes.js
+++ client/src/components/notes.js
@@ -4,7 +4,19 @@ import { Value } from 'slate';
 
 import '../assets/css/notes.css';
 
-const initialValue = Value.fromJSON({
+// start rich text toolbar part 1
+import { isKeyHotkey } from 'is-hotkey';
+
+const DEFAULT_NODE = 'paragraph';
+
+const isBoldHotkey = isKeyHotkey('mod+b');
+const isItalicHotkey = isKeyHotkey('mod+i');
+const isUnderlinedHotkey = isKeyHotkey('mod+u');
+const isCodeHotkey = isKeyHotkey('mod+`');
+// end rich text toolbar part 1
+
+const existingValue = JSON.parse(localStorage.getItem('content'));
+const initialValue = Value.fromJSON(existingValue || {
     document: {
         nodes: [
             {
@@ -23,25 +35,182 @@ const initialValue = Value.fromJSON({
             }
         ]
     }
-})
+});
 
 class Notes extends Component {
+
     state = {
         value: initialValue
     };
 
     onChange = ({ value }) => {
+        if (value.document !== this.state.value.document) {
+            const content = JSON.stringify(value.toJSON());
+            localStorage.setItem('content', content)
+        }
+
         this.setState({ value })
     };
 
+    // start rich text toolbar part 2
+    hasMark = (type) => {
+        const { value } = this.state;
+        return value.activeMarks.some(mark => mark.type === type)
+    };
+
+    hasBlock = (type) => {
+        const { value } = this.state;
+        return value.blocks.some(node => node.type === type)
+    };
+
+    onKeyDown = (event, change) => {
+        let mark;
+
+        if (isBoldHotkey(event)) {
+            mark = 'bold'
+        } else if (isItalicHotkey(event)) {
+            mark = 'italic'
+        } else if (isUnderlinedHotkey(event)) {
+            mark = 'underlined'
+        } else if (isCodeHotkey(event)) {
+            mark = 'code'
+        } else {
+            return
+        }
+
+        event.preventDefault();
+        change.toggleMark(mark);
+        return true
+    };
+
+    onClickMark = (event, type) => {
+        event.preventDefault();
+        const { value } = this.state;
+        const change = value.change().toggleMark(type);
+        this.onChange(change)
+    };
+
+    onClickBlock = (event, type) => {
+        event.preventDefault();
+        const { value } = this.state;
+        const change = value.change();
+        const { document } = value;
+
+        if (type !== 'bulleted-list' && type !== 'numbered-list') {
+            const isActive = this.hasBlock(type);
+            const isList = this.hasBlock('list-item');
+            if (isList) {
+                change
+                    .setBlock(isActive ? DEFAULT_NODE : type)
+                    .unwrapBlock('bulleted-list')
+                    .unwrapBlock('numbered-list')
+            } else {
+                change
+                    .setBlock(isActive ? DEFAULT_NODE : type)
+            }
+        } else {
+            const isList = this.hasBlock('list-item');
+            const isType = value.blocks.some((block) => {
+                return !!document.getClosest(block.key, parent => parent.type === type)
+            });
+
+            if (isList && isType) {
+                change
+                    .setBlock(DEFAULT_NODE)
+                    .unwrapBlock('bulleted-list')
+                    .unwrapBlock('numbered-list')
+            } else if (isList) {
+                change
+                    .unwrapBlock(type === 'bulleted-list' ? 'numbered-list' : 'bulleted-list')
+                    .wrapBlock(type)
+            } else {
+                change
+                    .setBlock('list-item')
+                    .wrapBlock(type)
+            }
+        }
+        this.onChange(change)
+    };
+
+    toolbar = () => {
+        return (
+            <div className="menu toolbar-menu">
+                {this.renderMarkButton('bold', 'format_bold')}
+                {this.renderMarkButton('italic', 'format_italic')}
+                {this.renderMarkButton('underlined', 'format_underlined')}
+                {this.renderMarkButton('code', 'code')}
+                {this.renderBlockButton('heading-one', 'looks_one')}
+                {this.renderBlockButton('heading-two', 'looks_two')}
+                {this.renderBlockButton('block-quote', 'format_quote')}
+                {this.renderBlockButton('numbered-list', 'format_list_numbered')}
+                {this.renderBlockButton('bulleted-list', 'format_list_bulleted')}
+            </div>
+        )
+    };
+
+    renderMarkButton = (type, icon) => {
+        const isActive = this.hasMark(type);
+        const onMouseDown = event => this.onClickMark(event, type);
+
+        return (
+            <span className="button" onMouseDown={onMouseDown} data-active={isActive}>
+            <span className="material-icons">{icon}</span>
+            </span>
+        )
+    };
+
+    renderBlockButton = (type, icon) => {
+        const isActive = this.hasBlock(type);
+        const onMouseDown = event => this.onClickBlock(event, type);
+
+        return (
+            <span className="button" onMouseDown={onMouseDown} data-active={isActive}>
+            <span className="material-icons">{icon}</span>
+            </span>
+        )
+    };
+
+    renderNode = (props) => {
+        const { attributes, children, node } = props;
+        switch (node.type) {
+            case 'block-quote': return <blockquote {...attributes}>{children}</blockquote>;
+            case 'bulleted-list': return <ul {...attributes}>{children}</ul>;
+            case 'heading-one': return <h1 {...attributes}>{children}</h1>;
+            case 'heading-two': return <h2 {...attributes}>{children}</h2>;
+            case 'list-item': return <li {...attributes}>{children}</li>;
+            case 'numbered-list': return <ol {...attributes}>{children}</ol>
+        }
+    };
+
+    renderMark = (props) => {
+        const { children, mark } = props;
+        switch (mark.type) {
+            case 'bold': return <strong>{children}</strong>;
+            case 'code': return <code>{children}</code>;
+            case 'italic': return <em>{children}</em>;
+            case 'underlined': return <u>{children}</u>
+        }
+    };
+    // end rich text toolbar part 2
+
     render() {
         return (
-            <Editor
-                value={this.state.value}
-                onChange={this.onChange}
-            />
+            <div className="notes-component">
+                <h1 className="notesTitle">Notes</h1>
+                {this.toolbar()}
+                <Editor
+                    placeholder="Enter some rich text..."
+                    value={this.state.value}
+                    onChange={this.onChange}
+                    onKeyDown={this.onKeyDown}
+                    renderNode={this.renderNode}
+                    renderMark={this.renderMark}
+                    spellCheck
+                />
+            </div>
         )
     }
+
 }
 
 export default Notes;
