bd30dcdb5e1e731e14c5d8970eb6af10fb3e8432
jauxsh <joshua.jih.chang@gmail.com>
2018-01-04 10:01:20 -0800

    added files that were outdated

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -3,7 +3,7 @@
 
 
 // //need to make that when link is clicked it takes to auth/google. server will return back to the page, need to have a switch conditional where it checks if the user logged in correctly
-// //and make it chage to log out button, i put a similar example on the bottom. 
+// //and make it chage to log out button, i put a similar example on the bottom.
 // //for now when clicked pops error. but after ~3 seconds it logs in but it desn't change to logged
 // function Login(){
 //     return (
@@ -21,9 +21,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-class Header extends Component{
-    renderContent(){
-        switch (this.props.auth){
+class Header extends Component {
+    renderContent() {
+        switch (this.props.auth) {
             case null:
                 return;
             case false:
@@ -34,27 +34,26 @@ class Header extends Component{
                 return <li><a href="/api/logout">Logout</a></li>;
         }
     }
-    
+
     render() {
         // console.log(this.props);
-        return(
-        <nav>
-            <div className="nav-wrapper">
-            {/* ternary expression  */}
-                <Link to={this.props.auth ? '/main' : '/'} className="left brand-logo"> 
-                FlexNote</Link>
-                <ul className="right">
-                    {this.renderContent()}
-                </ul>
-            </div>
-        </nav>
-        
+        return (
+            <nav>
+                <div className="nav-wrapper">
+                    {/* ternary expression  */}
+                    <Link to={this.props.auth ? '/main' : '/'} className="left brand-logo">
+                        FlexNote</Link>
+                    <ul className="right">
+                        {this.renderContent()}
+                    </ul>
+                </div>
+            </nav>
         );
     }
 
 }
-function mapStateToProps({ auth }){
-    return {auth};
+function mapStateToProps({ auth }) {
+    return { auth };
 }
 
 export default connect(mapStateToProps)(Header); 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,34 +1,25 @@
 const passport = require('passport');
 
 
-module.exports = app =>{
+module.exports = app => {
     app.get('/auth/google', passport.authenticate('google', {
         scope: ['profile', 'email']
     }));
 
     app.get('/auth/google/callback', passport.authenticate('google'),
-    (req, res)=>{
+        (req, res) => {
 
-        res.redirect('/main');
+            res.redirect('/main');
 
-    });
+        });
 
-    app.get('/api/logout', (req,res)=>{
+    app.get('/api/logout', (req, res) => {
         req.logout();
         res.redirect('/'); //nothing will be sent out since logged out
     });
 
-    app.get('/api/current_user', (req, res)=>{
-<<<<<<< HEAD
-        res.send(req.user); //req.session has what cookie has saved
-=======
+    app.get('/api/current_user', (req, res) => {
         console.log(req.user);
-        res.send(req.user); //req.session has what cookie has saved 
-<<<<<<< HEAD
->>>>>>> 6ca3dda076ccd400dbf0e94c1bd7ba52e694d8ea
+        res.send(req.user); //req.session has what cookie has saved
     })
 }
-=======
-    });
-};
->>>>>>> 534b50529c420f59b0dd52cd40603e15c51ad136
