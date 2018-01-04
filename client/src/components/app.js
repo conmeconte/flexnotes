534b50529c420f59b0dd52cd40603e15c51ad136
brucean52 <bruce.an52@gmail.com>
2018-01-03 18:32:35 -0800

    added dashboard

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -2,9 +2,10 @@ import React, {Component} from 'react';
 import { BrowserRouter, Route} from 'react-router-dom';
 import { connect} from 'react-redux';
 import * as actions from "../actions";
-import Panel from './panel';
-import NavBar from '../components/navbar/nav';
+//import Panel from './panel';
+//import NavBar from '../components/navbar/nav';
 import Login from './login';
+import Dashboard from './dashboard';
 import '../assets/css/app.css';
 
 class App extends Component {
@@ -17,7 +18,7 @@ class App extends Component {
             <div>
                <BrowserRouter >
                 <div className="container">
-                    <Route exact path="/main" component={Panel} />
+                    <Route exact path="/main" component={Dashboard} />
                     <Route exact path="/" component={Login} />
                         {/* // <NavBar/>
                         // <Panel/>
