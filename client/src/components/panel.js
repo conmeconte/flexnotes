5a07abac23bb5c72ab4c4a8cc0783a8e7170acc3
jauxsh <joshua.jih.chang@gmail.com>
2018-01-04 15:55:56 -0800

    able to grab size for each panels as numbers

--- client/src/components/panel.js
+++ client/src/components/panel.js
@@ -9,7 +9,43 @@ import '../assets/css/panel.css';
 import SplitPane from 'react-split-pane';
 
 class Panel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            vertical_width: 400,
+            horizontal_left: 400,
+            horizontal_right: 400,
+        };
+    }
+
+    logVWsize(panelSize) {
+        console.log("vw size: ", panelSize);
+
+        const { vertical_width } = this.state;
+        this.setState({
+            vertical_width: panelSize
+        });
+    }
+
+    logHLsize(panelSize) {
+        console.log("hl size: ", panelSize);
+
+        const { horizontal_left } = this.state;
+        this.setState({
+            horizontal_left: panelSize
+        });
+    }
+
+    logHRsize(panelSize) {
+        console.log("hr size: ", panelSize);
+
+        const { horizontal_right } = this.state;
+        this.setState({
+            horizontal_right: panelSize
+        });
+    }
     render() {
+        const { vertical_width, horizontal_left, horizontal_right } = this.state;
         return (
             <div>
                 {/* <SplitPane split="vertical" defaultSize={200}>
@@ -18,15 +54,15 @@ class Panel extends Component {
                     <div><Slides/></div>
                     <div>MeisterTask</div>
                 </SplitPane> */}
-                <SplitPane split="vertical" minSize={200} defaultSize={400}>
-                    <SplitPane split="horizontal" minSize={200} defaultSize={400}>
-                        <div><Slides/></div>
-                        <div><Video/></div>
-                     </SplitPane>
-                     <SplitPane split="horizontal" minSize={200} defaultSize={400}>
-                        <div className="notes-parent-panel"><Notes/></div>
+                <SplitPane onChange={size => this.logVWsize(size)} split="vertical" minSize={200} maxSize={-200} defaultSize={vertical_width}>
+                    <SplitPane onChange={size => this.logHLsize(size)} split="horizontal" minSize={200} maxSize={-200} defaultSize={horizontal_left}>
+                        <div><Slides /></div>
+                        <div><Video /></div>
+                    </SplitPane>
+                    <SplitPane onChange={size => this.logHRsize(size)} split="horizontal" minSize={200} maxSize={-200} defaultSize={horizontal_right}>
+                        <div className="notes-parent-panel"><Notes /></div>
                         <div>MeisterTask</div>
-                     </SplitPane>
+                    </SplitPane>
                 </SplitPane>
             </div>
         );
