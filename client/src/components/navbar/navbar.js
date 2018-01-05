53745e60884bd1317c9f1bc5477132b3742c162c
jauxsh <joshua.jih.chang@gmail.com>
2018-01-05 10:05:08 -0800

    committing changes

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -2,17 +2,17 @@ import React from 'react';
 import Nav from './nav';
 
 export default props => {
-    return(
+    return (
         <div className="navbar col-xs-2">
-            <Nav/>
+            <Nav />
             <div className="contain-tab">
                 <h4 className="nav_header">Tabs</h4>
-                <hr/>
+                <hr />
             </div>
             <div className="contain-page">
                 <h4 className="nav_header">Pages</h4>
-                <hr/>
-            </div>   
+                <hr />
+            </div>
         </div>
     );
 }
