133f28c9f66dd2c04f1ff50cb93a366b43b60454
dazcha76 <dazcha@hotmail.com>
2018-01-08 10:56:24 -0800

    updated node, imported axios

--- client/src/components/notes.js
+++ client/src/components/notes.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 import { Editor, getEventRange, getEventTransfer } from 'slate-react';
 import { Block, Value } from 'slate';
 import { isKeyHotkey } from 'is-hotkey';
