d7fefeb1c1ae07766349b0bcb276d3395bfc6d23
rslibed <rslibed@gmail.com>
2018-01-08 16:32:27 -0800

    Added icon to logout button and absolutely positioned to right, still need to work on button to save panel dimensions

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -1,13 +1,15 @@
 import React from 'react';
-
 import Panel from './panel';
 import NavBar from './navbar/navbar';
 import Login from './login';
+import '../assets/css/login.css';
 
 export default props => {
     return (
         <div>
-            <Login />
+            <div className="login">
+                <Login />
+            </div>
             <NavBar />
             <Panel />
         </div>
