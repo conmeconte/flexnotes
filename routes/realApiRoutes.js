7a66113b46bd775c79aac4d842bef2446fd337bd
conmeconte <saja523@hotmail.com>
2018-01-09 14:18:50 -0800

    accessing nested arrays and objectsgit add .

--- routes/realApiRoutes.js
+++ routes/realApiRoutes.js
@@ -11,29 +11,8 @@ module.exports = (app, db) => {
     app.get('/api/userInfo', requireLogin, (req, res) => {
 
         res.send(req.user);
-    })
-    app.get('/api/dummyData', requireLogin, (req, res) => {
-        res.send(dummyData);
+    });
 
-    })
-    // app.get('/dummyData', (req,res)=>{
-    //     if(req.user.userName=== 'JOhn Hong'){
-    //         dummyData.binder_arr_obj.forEach(ele=>{
-    //             if(ele.binder_id === 1){
-    //                 console.log('if passed')
-    //                 ele.binder_name= "binder name changed";
-    //                 res.send(dummyData);
-    //             }
-    //         })
-    //     }
-
-
-    // })
-    app.post('/api/dummyData', (req, res) => {
-
-
-        res.send(dummyData);
-    })
 
     app.get('/api', requireLogin, async (req, res) => {
         //pull entire user obj
@@ -41,10 +20,19 @@ module.exports = (app, db) => {
     })
     // For Binder //
     app
-        .get('/api/binder', requireLogin, async (req, res) => {
+        .get('/api/binder', async (req, res) => {
             //give binder data
             //userId accessible via req.param.userId?
-            console.log('you reached here', req.user)
+            const existingUser= await User.findOne({ 'googleId': "103970352561814947806" }, function (err, user){
+                if (err) { res.send("Error did occurred") };
+
+                if (user) {
+                    res.send(user);
+                }else {
+                res.send("Error can't find user")
+                }
+                res.end();
+            })
         })
         .post('/api/binder', async (req, res) => {
             //create new binder in user
@@ -54,7 +42,8 @@ module.exports = (app, db) => {
                 if (err) { res.send("Error did occurred") };
 
                 if (user) {
-                    const defaultBinder = new Binder();
+                    let idRandomizer= Math.floor((Math.random()* 99999999999) + 00000000000);
+                    const defaultBinder = new Binder({binder_id: idRandomizer});
                     // const prevBinderId= user.binder_arr_obj[binder_arr_obj.length-1].binder_id; 
                     // defaultBinder.binder_id= 
                     defaultBinder.tab_arr_obj.push(new Tab());
@@ -76,23 +65,27 @@ module.exports = (app, db) => {
             res.end();
         })
         .delete('/api/binder', async (req, res) => {
-            //delete binder
-            const existingUser = await User.findOne({ 'googleId': "103970352561814947806" }, function (err, user) {
-                if (err) { res.send("Error Occurred") };
-                if (user) {
-                    user.binder_arr_obj.pop();
-                    res.send(user.save());
-                    console.log('a binder has been deleted');
-                } else {
-                    res.status(404).end();
-                }
-
+            const existingUser= await User.update({ 'googleId': "103970352561814947806",'binder_arr_obj.binder_id': req.body.binder_id  }, {'$pull':{'binder_arr_obj':{'binder_id': req.body.binder_id}}},(err,data)=>{
+                console.log(data);
+                console.log(err);
             })
+            
+            // const existingUser = await User.findOne({ 'googleId': "103970352561814947806" }, function (err, user) {
+            //     if (err) { res.send("Error Occurred") };
+            //     if (user) {
+            //         user.binder_arr_obj.pop();
+            //         res.send(user.save());
+            //         console.log('a binder has been deleted');
+            //     } else {
+            //         res.status(404).end();
+            //     }
+
+            // })
             res.end();
         })
         .put('/api/binder', async (req, res) => {
             // update binder
-            User.update({"googleId": req.body.googleId,'binder_arr_obj.binder_id': req.body.binder_id }, {'$set':{"binder_arr_obj.$.binder_name": "Sparta"}
+            User.update({"googleId": "103970352561814947806",'binder_arr_obj.binder_id': "B-01" }, {'$set':{"binder_arr_obj.$.binder_name": "Samurai"}
             }, (err, data)=>{
                 console.log(err);
                 console.log(data);
@@ -132,39 +125,71 @@ module.exports = (app, db) => {
 
 
     // For Tab//
-    app.get('/api/tab', requireLogin, async (req, res) => {
+    // For Tab//
+    // For Tab//
+    // For Tab//
+    app.get('/api/tab', async (req, res) => {
         //give tab data
         // www.chung.com/user/1/binder/4/tab/3
-    });
-    app.post('/api/tab', async (req, res) => {
-        //create new tab in user
-        const existingUser = await User.findOne({ 'googleId': "103970352561814947806" }, function (err, user) {
-
-            if (err) { res.send("Error Occurred") }
+        const existingUser= await User.findOne({ 'googleId': "103970352561814947806" }, function (err, user){
+            if (err) { res.send("Error did occurred") };
 
             if (user) {
-                const defaultTab = new Tab();
-                // const prevBinderId= user.binder_arr_obj[binder_arr_obj.length-1].binder_id; 
-                // defaultBinder.binder_id= 
-                defaultTab.page_arr_obj.push(new Page());
-                defaultTab.page_arr_obj[0].video.push(new Video({ videoInfo: 'No Info' }));
-                defaultTab.page_arr_obj[0].notes.document.nodes.push(new Note());
-                user.binder_arr_obj[0].tab_arr_obj.push(defaultTab);  //binder_arr_obj[num] num should be whichever binder that called the method, might need to search for id number
-                user.save((err, data) => {
-                    if (err) {
-                        console.log('some error');
-                    }
-                    if (data) {
-                        res.send(data);
-                        console.log("User has a new tab and is now saved");
-                    } else {
-                        console.log('user not found');
-                    }
-                })
-            } else {
-                res.send("Error can't find user")
+                res.send(user);
+            }else {
+            res.send("Error can't find user")
             }
+            res.end();
+    
         });
+    })
+    app.post('/api/tab', async (req, res) => {
+        //create new tab in user
+        let idRandomizer= Math.floor((Math.random()* 99999999999) + 00000000000);
+        const defaultTab = new Tab({tab_id: idRandomizer});
+        defaultTab.page_arr_obj.push(new Page());
+        defaultTab.page_arr_obj[0].video.push(new Video({ videoInfo: 'No Info' }));
+        defaultTab.page_arr_obj[0].notes.document.nodes.push(new Note());
+        
+        
+        const existingUser= await User.update({ 'googleId': "103970352561814947806",'binder_arr_obj.binder_id': req.body.binder_id  }, 
+        {'$push':{'binder_arr_obj':{"tab_arr_obj":defaultTab}}},(err,data)=>{
+            console.log(data);
+            console.log(err);
+        })
+
+        res.end();
+        
+        
+        
+        
+        
+        // const existingUser = await User.findOne({ 'googleId': "103970352561814947806" }, function (err, user) {
+        //     if (err) { res.send("Error Occurred") }
+
+        //     if (user) {
+        //         const defaultTab = new Tab();
+        //         // const prevBinderId= user.binder_arr_obj[binder_arr_obj.length-1].binder_id; 
+        //         // defaultBinder.binder_id= 
+        //         defaultTab.page_arr_obj.push(new Page());
+        //         defaultTab.page_arr_obj[0].video.push(new Video({ videoInfo: 'No Info' }));
+        //         defaultTab.page_arr_obj[0].notes.document.nodes.push(new Note());
+        //         user.binder_arr_obj[0].tab_arr_obj.push(defaultTab);  //binder_arr_obj[num] num should be whichever binder that called the method, might need to search for id number
+        //         user.save((err, data) => {
+        //             if (err) {
+        //                 console.log('some error');
+        //             }
+        //             if (data) {
+        //                 res.send(data);
+        //                 console.log("User has a new tab and is now saved");
+        //             } else {
+        //                 console.log('user not found');
+        //             }
+        //         })
+        //     } else {
+        //         res.send("Error can't find user")
+        //     }
+        // });
 
     });
     app.delete('/api/tab', requireLogin, async (req, res) => {
@@ -177,8 +202,17 @@ module.exports = (app, db) => {
     // For Page //
 
     app.get('/api/page', async (req,res)=>{
-        console.log(req.user);
-        res.send(dummyData);
+        const existingUser= await User.findOne({ 'googleId': "103970352561814947806" }, function (err, user){
+            if (err) { res.send("Error did occurred") };
+
+            if (user) {
+                res.send(user);
+            }else {
+            res.send("Error can't find user")
+            }
+            res.end();
+    
+        });
     });
     app.post('/api/page', async (req, res) => {
         //create new page in user
