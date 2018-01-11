7ec39733352098551b9e613b6a88690d79f04ed9
conmeconte <32534202+conmeconte@users.noreply.github.com>
2018-01-10 23:58:03 -0800

    video part finished, moving on to notes but need to talk to danika

--- routes/realApiRoutes.js
+++ routes/realApiRoutes.js
@@ -16,7 +16,8 @@ module.exports = (app, db) => {
 
     app.get('/api', async (req, res) => {
         //pull entire user obj
-        User.findById(req.user.id, (err,user)=>{
+        User.findById(req.body.id, (err,user)=>{
+        // User.findById(req.user.id, (err,user)=>{
             res.send(user);
         })
 
@@ -26,7 +27,8 @@ module.exports = (app, db) => {
         .get('/api/binder', async (req, res) => {
             //give binder data
             //userId accessible via req.param.userId?
-            const existingUser= await User.findOne({ 'googleId': req.user.googleId }, function (err, user){
+            const existingUser= await User.findById(req.body.id, function (err, user){
+            // const existingUser= await User.findById(req.user.id, function (err, user){
                 if (err) { res.send("Error did occurred") };
 
                 if (user) {
@@ -38,12 +40,13 @@ module.exports = (app, db) => {
             })
         })
         .post('/api/binder', async (req, res) => {
-            const existingUser= await User.findById(req.user.id,function(err,user){
+            const existingUser= await User.findById(req.body.id,function(err,user){
+            // const existingUser= await User.findById(req.user.id,function(err,user){
                 if (err) { res.send("Error did occurred") };
     
                 if (user) {
                     const binder = user
-                    .binder_arr_obj.id('5a56b2782962901f9c0fe0fd') //req.body.binder_arr_obj_id
+                    .binder_arr_obj.id(req.body.binder_arr_obj_id) //req.body.binder_arr_obj_id
                     if(binder){
                         const defaultBinder = new Binder();
                         defaultBinder.tab_arr_obj.push(new Tab());
@@ -62,7 +65,8 @@ module.exports = (app, db) => {
 
         })
         .delete('/api/binder', async (req, res) => {
-            const existingUser= await User.findById(req.user.id,function(err,user){
+            const existingUser= await User.findById(req.binder.id,function(err,user){
+            // const existingUser= await User.findById(req.user.id,function(err,user){
                 if (err) { res.send("Error did occurred") };
     
                 if (user) {
@@ -80,7 +84,8 @@ module.exports = (app, db) => {
         })
         .put('/api/binder', async (req, res) => {
             // update binder
-            const existingUser= await User.findById(req.user.id, function (err, user){
+            const existingUser= await User.findById(req.body.id, function (err, user){
+            // const existingUser= await User.findById(req.user.id, function (err, user){
                 if (err) { res.send("Error did occurred")};
     
                 if (user) {
@@ -106,7 +111,8 @@ module.exports = (app, db) => {
     // For Tab//
     app
         .get('/api/tab', async (req, res) => {
-            const existingUser= await User.findOne({ 'googleId': "103970352561814947806" }, function (err, user){
+            const existingUser= await User.findById(req.body.id, function (err, user){
+            // const existingUser= await User.findById(req.user.id, function (err, user){
                 if (err) { res.send("Error did occurred")};
 
                 if (user) {
@@ -120,12 +126,13 @@ module.exports = (app, db) => {
         })
         .post('/api/tab', async (req, res) => {
             //create new tab in user
-            const existingUser= await User.findById(req.user.id,function(err,user){
+            const existingUser= await User.findById(req.body.id,function(err,user){
+            // const existingUser= await User.findById(req.user.id,function(err,user){
                 if (err) { res.send("Error did occurred") };
 
                 if (user) {
                     const binder = user
-                    .binder_arr_obj.id('5a56b2782962901f9c0fe0fd') //req.body.binder_arr_obj_id
+                    .binder_arr_obj.id(req.body.binder_arr_obj_id) //req.body.binder_arr_obj_id
                     if(binder){
                         tab= new Tab();
                         tab.page_arr_obj.push(new Page());
@@ -143,13 +150,14 @@ module.exports = (app, db) => {
             })
         })
         .delete('/api/tab', async (req, res) => {
-            const existingUser= await User.findById('5a569b34da2d998e141c38b2',function(err,user){
+            const existingUser= await User.findById(req.body.id,function(err,user){
+            // const existingUser= await User.findById(req.user.id,function(err,user){
                 if (err) { res.send("Error did occurred") };
 
                 if (user) {
                     const tab = user
-                    .binder_arr_obj.id('5a569b34da2d998e141c38ad') //req.body.binder_arr_obj_id
-                    .tab_arr_obj.id('5a56b09af2218b0b10dbd8a0')  //req.body.tab_arr_obj_id
+                    .binder_arr_obj.id(req.body.binder_arr_obj_id) //req.body.binder_arr_obj_id
+                    .tab_arr_obj.id(req.body.tab_arr_obj_id)  //req.body.tab_arr_obj_id
                     
                     tab.remove();
                     user.save();
@@ -162,13 +170,14 @@ module.exports = (app, db) => {
         })
         .put('/api/tab', async (req, res) => {
 
-            const existingUser= await User.findById('5a569b34da2d998e141c38b2', function (err, user){
+            const existingUser= await User.findById(req.body.id, function (err, user){
+            // const existingUser= await User.findById(req.user.id, function (err, user){
                 if (err) { res.send("Error did occurred")};
 
                 if (user) {
                     const tab = user
-                    .binder_arr_obj.id('5a569b34da2d998e141c38ad') //req.body.binder_arr_obj_id
-                    .tab_arr_obj.id('5a569b34da2d998e141c38ae')  //req.body.tab_arr_obj_id
+                    .binder_arr_obj.id(req.body.binder_arr_obj_id) //req.body.binder_arr_obj_id
+                    .tab_arr_obj.id(req.body.tab_arr_obj_id)  //req.body.tab_arr_obj_id
                     
                     tab.tab_color = req.body.tab_color || tab.tab_color;
                     tab.tab_name = req.body.tab_name || tab.tab_name;
@@ -188,7 +197,8 @@ module.exports = (app, db) => {
 
     app
         .get('/api/page', async (req,res)=>{
-            const existingUser= await User.findOne({ 'googleId': "103970352561814947806" }, function (err, user){
+            const existingUser= await User.findById(req.body.id, function (err, user){
+            // const existingUser= await User.findById(req.user.id, function (err, user){
                 if (err) { res.send("Error did occurred") };
 
                 if (user) {
@@ -202,13 +212,14 @@ module.exports = (app, db) => {
         })
         .post('/api/page', async (req, res) => {
             //create new page in user
-            const existingUser= await User.findById('5a56b2782962901f9c0fe102',function(err,user){
+            const existingUser= await User.findById(req.body.id,function(err,user){
+            // const existingUser= await User.findById(req.user.id,function(err,user){
                 if (err) { res.send("Error did occurred") };
 
                 if (user) {
                     const tab = user
-                    .binder_arr_obj.id('5a56b64553169388cce1767f') //req.body.binder_arr_obj_id
-                    .tab_arr_obj.id('5a56b64553169388cce17680')  //req.body.tab_arr_obj_id
+                    .binder_arr_obj.id(req.body.binder_arr_obj_id) //req.body.binder_arr_obj_id
+                    .tab_arr_obj.id(req.body.tab_arr_obj_id)  //req.body.tab_arr_obj_id
                     if(tab){
                         let page = new Page()
                         page.video.push(new Video());
@@ -231,9 +242,9 @@ module.exports = (app, db) => {
 
                 if (user) {
                     const page = user
-                    .binder_arr_obj.id('5a569b34da2d998e141c38ad') //req.body.binder_arr_obj_id
-                    .tab_arr_obj.id('5a569b34da2d998e141c38ae')  //req.body.tab_arr_obj_id
-                    .page_arr_obj.id('5a56ac350fc70b12841bf53f'); //req.body.page_arr_obj_id
+                    .binder_arr_obj.id(req.body.binder_arr_obj_id) //req.body.binder_arr_obj_id
+                    .tab_arr_obj.id(req.body.tab_arr_obj_id)  //req.body.tab_arr_obj_id
+                    .page_arr_obj.id(req.body.page_arr_obj_id); //req.body.page_arr_obj_id
                     
                     page.remove();
                     user.save();
@@ -272,7 +283,8 @@ module.exports = (app, db) => {
  //video//
     app
         .post('/api/video', async (req,res)=>{
-            const existingUser= await User.findById(req.user.id, (err,user)=>{
+            const existingUser= await User.findById(req.body.id, (err,user)=>{
+            // const existingUser= await User.findById(req.user.id, (err,user)=>{
                 if(err){ res.send('Error')}
                 if (user) {
                     const page = user
@@ -280,10 +292,8 @@ module.exports = (app, db) => {
                     .tab_arr_obj.id(req.body.tabID)  
                     .page_arr_obj.id(req.body.pageID)
                     if(page){
-                        let video = new Video()
-                        page.video.push(new Video({video_url: req.body.video_url, videoInfo: req.body.videoInfo}));
-                        page.notes.document.nodes.push(new Note());
-                        tab.page_arr_obj.push(page);
+                        page.video.push(new Video({vid_url: req.body.vid_url, videoInfo: req.body.videoInfo}));
+
 
                         user.save();
                         res.send(user);
@@ -295,7 +305,8 @@ module.exports = (app, db) => {
             })
         })
         .delete('/api/video', async (req,res)=>{
-            const existingUser= await User.findById(req.user.id, (err,user)=>{
+            const existingUser= await User.findById(req.body.id, (err,user)=>{
+            // const existingUser= await User.findById(req.user.id, (err,user)=>{
                 if(err){ res.send('Error')}
                 if (user) {
                     const video = user
@@ -317,6 +328,7 @@ module.exports = (app, db) => {
         })
         .put('/api/video', async (req,res)=>{
             const existingUser= await User.findById(req.body.id, (err,user)=>{
+            // const existingUser= await User.findById(req.user.id, (err,user)=>{
                 if(err){ res.send('Error')}
                 if (user) {
                     const video = user
@@ -338,5 +350,81 @@ module.exports = (app, db) => {
             })
         });
 
+//note//
+app
+    .post('/api/note', async (req,res)=>{
+        const existingUser= await User.findById(req.body.id, (err,user)=>{
+        // const existingUser= await User.findById(req.user.id, (err,user)=>{
+            if(err){ res.send('Error')}
+            if (user) {
+                const page = user
+                .binder_arr_obj.id(req.body.binderID)
+                .tab_arr_obj.id(req.body.tabID)  
+                .page_arr_obj.id(req.body.pageID)
+                if(page){
+                    page.notes.nodes.push(req.body.notesNode);
+
+
+                    user.save();
+                    res.send(user);
+                }else{res.send('wrong path')}
+                
+            }else {
+            res.send("Error can't find user")
+            }
+        })
+    })
+    // .delete('/api/note', async (req,res)=>{
+    //     const existingUser= await User.findById(req.body.id, (err,user)=>{
+    //     // const existingUser= await User.findById(req.user.id, (err,user)=>{
+    //         if(err){ res.send('Error')}
+    //         if (user) {
+    //             const video = user
+    //             .binder_arr_obj.id(req.body.binderID)
+    //             .tab_arr_obj.id(req.body.tabID)  
+    //             .page_arr_obj.id(req.body.pageID)
+    //             .video.id(req.body.videoID)
+    //             if(video){
+    //                 video.remove();
+    //                 user.save();
+    //                 res.send(user);
+    //             }else{res.send('wrong path')}
+                
+    //         }else {
+    //             res.status(500);
+    //             res.render('error', {error: err}).send("Error can't find user")
+    //         }
+    //     })
+    // })
+    // .put('/api/note', async (req,res)=>{
+    //     const existingUser= await User.findById(req.body.id, (err,user)=>{
+    //     // const existingUser= await User.findById(req.user.id, (err,user)=>{
+    //         if(err){ res.send('Error')}
+    //         if (user) {
+    //             const video = user
+    //             .binder_arr_obj.id(req.body.binderID)
+    //             .tab_arr_obj.id(req.body.tabID)  
+    //             .page_arr_obj.id(req.body.pageID)
+    //             .video.id(req.body.videoID)
+    //             if(video){
+    //                 video.vid_url= req.body.vid_url || video.vid_url;
+    //                 video.videoInfo= req.body.videoInfo || video.videoInfo;
+    //                 user.save();
+    //                 res.send(user);
+    //             }else{res.send('wrong path')}
+                
+    //         }else {
+    //             res.status(500);
+    //             res.render('error', {error: err}).send("Error can't find user")
+    //         }
+    //     })
+    // });
+
+
+
 
 }
+
+
+
+
