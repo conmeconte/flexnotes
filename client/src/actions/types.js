27d8bca8a9ba6cdc36d5958e4fd947243e267c4e
jauxsh <joshua.jih.chang@gmail.com>
2018-01-12 18:02:03 -0800

    committed this one before getting latest dev

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,6 +1,6 @@
 export const FETCH_USER = 'fetch_user';
 export default {
-    FETCH_USER : 'fetch_user',
+    FETCH_USER: 'fetch_user',
     BINDER_ARRAY: 'binder_array',
     ADD_BINDER: 'add_binder',
     SELECT_BINDER: 'select_binder',
@@ -10,7 +10,8 @@ export default {
     GET_RESULT_STYLES: 'get_result_styles',
     GET_OPACITY_DISPLAY: 'get_opacity_display',
     TOGGLE_RESULTS: 'toggle_results',
-    ADD_TO_PLAYLIST : 'add_to_playlist',
+    ADD_TO_PLAYLIST: 'add_to_playlist',
     PLAY_VIDEO: 'play_video',
-    GRAB_VIDEO_URL: 'grab_video_url'
+    GRAB_VIDEO_URL: 'grab_video_url',
+    SET_SLIDES_URL: 'set_slides_url',
 }
---
+++ client/src/reducers/slide_reducer.js
@@ -0,0 +1,12 @@
+import types from '../actions/types';
+
+const DEFAULT_STATE = {
+
+};
+
+export default funcitong(state = DEFAULT_STATE, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
