255e8d25e656c4ea48b2d4cc332698157775acd0
jauxsh <joshua.jih.chang@gmail.com>
2018-01-18 15:40:54 -0800

    added functionality to reload panel sizes from db

--- client/src/components/panel_three.js
+++ client/src/components/panel_three.js
@@ -24,11 +24,6 @@ class ThreePanel extends Component {
         this.props.setTopLeftWidth(size, this.props.interface_obj);
     }
 
-    getSize() {
-        console.log('Get size works!');
-        return 500;
-    }
-
     render() {
         const loTLHsave = _.debounce((size) => {
             this.logTopLeftHeight(size);
@@ -38,9 +33,34 @@ class ThreePanel extends Component {
             this.logTopLeftWidth(size);
         }, 300);
 
+        let tabArrLength = this.props.binderObj.tab_arr_obj.length;
+        let tabIndex = null;
+        let pageIndex = null;
+        let topLeftPanelHeight;
+        let topLeftPanelWidth;
+        for (let i = 0; i < tabArrLength; i++) {
+            if (this.props.interface_obj.tab_id === this.props.binderObj.tab_arr_obj[i]._id) {
+                tabIndex = i;
+                break;
+            }
+        }
+        const { page_arr_obj } = this.props.binderObj.tab_arr_obj[tabIndex];
+        for (let i = 0; i < tabArrLength; i++) {
+            if (this.props.interface_obj.page_id === page_arr_obj[i]._id) {
+                pageIndex = i;
+                break;
+            }
+        }
+        if (typeof page_arr_obj[pageIndex].panel_dimensions === 'undefined') {
+            topLeftPanelHeight = 500;
+            topLeftPanelWidth = 425;
+        } else {
+            topLeftPanelHeight = page_arr_obj[pageIndex].panel_dimensions.top_left_panel_height;
+            topLeftPanelWidth = page_arr_obj[pageIndex].panel_dimensions.top_left_panel_width;
+        }
         return (
-            <SplitPane onChange={loTLHsave} className="width-w-nav" split="vertical" minSize={200} maxSize={-200} defaultSize={ () => this.getSize}>
-                <SplitPane onChange={loTLWsave} split="horizontal" minSize={200} maxSize={-200} defaultSize={425}>
+            <SplitPane onChange={loTLHsave} className="width-w-nav" split="vertical" minSize={200} maxSize={-200} defaultSize={topLeftPanelHeight}>
+                <SplitPane onChange={loTLWsave} split="horizontal" minSize={200} maxSize={-200} defaultSize={topLeftPanelWidth}>
                     <div className="video-parent-panel"><Video /></div>
                 <div className="slides-container"><Slides /></div>
                 </SplitPane>
@@ -54,7 +74,8 @@ function mapStateToProps(state) {
     return {
         tlh: state.panelSpecs.topLeftHeight,
         tlw: state.panelSpecs.topLeftWidth,
-        interface_obj: state.interface
+        interface_obj: state.interface,
+        binderObj: state.binder.binderObj,
     }
 }
 
