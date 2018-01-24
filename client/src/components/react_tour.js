945fb61e4202a90dbf0795296d41970c17f69522
dazcha76 <dazcha@hotmail.com>
2018-01-24 11:08:38 -0800

    cleaned up code

--- client/src/components/react_tour.js
+++ client/src/components/react_tour.js
@@ -1,35 +1,10 @@
-import React, { Component } from 'react';
-// import Tour from 'reactour';
+import React from 'react';
 
 function FlexNotesTour({toggleTour}){
     return(
         <button className="startTour" onClick={toggleTour}>Tour</button>
     )
 }
-
-// class FlexNotesTour extends Component {
-//     constructor(props){
-//         super(props);
-//
-//         this.state = {
-//             isTourOpen: false
-//         }
-//     }
-//
-//     closeTour = () => {
-//         this.setState({ isTourOpen: false });
-//     };
-//
-//     openTour = () => {
-//         this.setState({ isTourOpen: true });
-//     };
-//
-//     render(){
-//         return(
-//             <button className="startTour" onClick={this.openTour}>Tour</button>
-//         )
-//     }
-// }
 
 export default FlexNotesTour;
 
