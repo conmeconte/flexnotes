78a82dac07e5dd0e0bceceebfc2c315b2f01bc00
rslibed <rslibed@gmail.com>
2018-02-18 12:29:31 -0800

    Removed console logs and unused destructured input properties

--- client/src/components/video-container.js
+++ client/src/components/video-container.js
@@ -3,106 +3,138 @@ import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import * as actions from '../actions';
 
-
-
 class VideoContainer extends Component {
-    renderInput({ input, type, placeholder, defaultValue, meta: { error, touched } }) {
-        return (
-            <div className="col s8 input-field">
-                <input {...input} className="pastedVideoInput" type={type} placeholder="Paste and Save a Youtube video URL..." value={input.value === '' && defaultValue ? defaultValue : input.value} />
-                <p className="red-text"><em>{touched && error ? error : ''}</em></p>
-            </div>
-        );
-    }
+  renderInput({ input, type, meta: { error, touched } }) {
+    return (
+      <div className="col s8 input-field">
+        <input
+          {...input}
+          className="pastedVideoInput"
+          type={type}
+          placeholder="Paste and Save a Youtube video URL..."
+          value={input.value}
+        />
+        <p className="red-text">
+          <em>
+            {touched && error ? error : ''}
+          </em>
+        </p>
+      </div>
+    );
+  }
 
-    handleYouTubeUrl(values) {
-        const youtubeLinkInput = values["youtube-url"];
-        if (!youtubeLinkInput || youtubeLinkInput.indexOf("youtu") === -1) {
-            return;
-        }
-        // console.log(values.input);
-        // if (!value) {
-        //     console.log("PLEASE SEARCH A VIDEO.");
-        //     return;
-        // }
-        // this.props.grabVideoUrl(values.input);
-        this.props.playPastedLinkVideo(values["youtube-url"]);
-        this.props.addToPlaylist(values["youtube-url"], '', this.props.binderTabPageIds);
-        //this.props.getDataObject();
-        //this.props.updateBinderArray();
+  handleYouTubeUrl(values) {
+    const youtubeLinkInput = values['youtube-url'];
+    if (!youtubeLinkInput || youtubeLinkInput.indexOf('youtu') === -1) {
+      return;
     }
+    this.props.playPastedLinkVideo(values['youtube-url']);
+    this.props.addToPlaylist(
+      values['youtube-url'],
+      '',
+      this.props.binderTabPageIds
+    );
+  }
 
-    render() {
-        return (
-            <div className="iframe-wrapper">
-                <div className="row">
-                    <form onSubmit={this.props.handleSubmit(this.handleYouTubeUrl.bind(this))}>
-                        <div style={this.props.slideOutStyles} className="row slide-out-input">
-                            <Field name="youtube-url" component={this.renderInput} />
-                            <div className="col s3">
-                                <div className="row btn-wrapper">
-                                    <button className="btn btn-success green darken-1 video-btn"><i className="material-icons">check</i></button>
-                                    <button type="button" className="btn btn-primary vidList vid-left-arrow video-btn" onClick={() => {
-                                        this.props.getResultStyles(this.props.resultsStyles, this.props.toggleResultsBool)
-                                        this.props.getOpacityDisplay(this.props.opacityContainer, this.props.toggleResultsBool)
-                                    }}><i className="fa fa-youtube" aria-hidden="true"></i>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                    <div className="arrow-container" onClick={() => {
-                        this.props.slideOutVideoSearch(this.props.toggleSlideOut, this.props.slideOutStyles);
-                    }}>
-                        {!this.props.toggleSlideOut ? <i className="material-icons">keyboard_arrow_up</i> : <i className="material-icons">keyboard_arrow_down</i>}
-                    </div>
+  render() {
+    return (
+      <div className="iframe-wrapper">
+        <div className="row">
+          <form
+            onSubmit={this.props.handleSubmit(this.handleYouTubeUrl.bind(this))}
+          >
+            <div
+              style={this.props.slideOutStyles}
+              className="row slide-out-input"
+            >
+              <Field name="youtube-url" component={this.renderInput} />
+              <div className="col s3">
+                <div className="row btn-wrapper">
+                  <button className="btn btn-success green darken-1 video-btn">
+                    <i className="material-icons">check</i>
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-primary vidList vid-left-arrow video-btn"
+                    onClick={() => {
+                      this.props.getResultStyles(
+                        this.props.resultsStyles,
+                        this.props.toggleResultsBool
+                      );
+                      this.props.getOpacityDisplay(
+                        this.props.opacityContainer,
+                        this.props.toggleResultsBool
+                      );
+                    }}
+                  >
+                    <i className="fa fa-youtube" aria-hidden="true" />
+                  </button>
                 </div>
-                <div id="video-container" className="video-container">
-                    <div className="video-embed-wrapper">
-                        <div className="resize-blocker"></div>
-                        <iframe allowFullScreen id="video-iframe" src={this.props.videoLink} className="video-iframe"></iframe>
-                    </div>
-                </div>
+              </div>
             </div>
-        )
-    }
+          </form>
+          <div
+            className="arrow-container"
+            onClick={() => {
+              this.props.slideOutVideoSearch(
+                this.props.toggleSlideOut,
+                this.props.slideOutStyles
+              );
+            }}
+          >
+            {!this.props.toggleSlideOut
+              ? <i className="material-icons">keyboard_arrow_up</i>
+              : <i className="material-icons">keyboard_arrow_down</i>}
+          </div>
+        </div>
+        <div id="video-container" className="video-container">
+          <div className="video-embed-wrapper">
+            <div className="resize-blocker" />
+            <iframe
+              allowFullScreen
+              id="video-iframe"
+              src={this.props.videoLink}
+              className="video-iframe"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-    return {
-        pastedVideoUrl: state.videoResults.videoLink,
-        videoLink: state.video.videoLink,
-        binderTabPageIds: state.interface,
-        playlist: state.videoResults.playlist,
-        addVideoModalStyle: state.video.addVideoModal,
-        videoTitle: state.video.videoTitle,
-        resultsStyles: state.video.resultsStyles,
-        toggleResultsBool: state.video.toggleResults,
-        opacityContainer: state.video.opacityDisplay,
-        interface_obj: state.interface,
-        slideOutStyles: state.video.videoLinkSlideOut,
-        toggleSlideOut: state.video.toggleSlideOut,
-        url: state.url,
-        // pastedVideoUrl: state.videoResults.videoLink,
-    }
+  return {
+    pastedVideoUrl: state.videoResults.videoLink,
+    videoLink: state.video.videoLink,
+    binderTabPageIds: state.interface,
+    playlist: state.videoResults.playlist,
+    addVideoModalStyle: state.video.addVideoModal,
+    videoTitle: state.video.videoTitle,
+    resultsStyles: state.video.resultsStyles,
+    toggleResultsBool: state.video.toggleResults,
+    opacityContainer: state.video.opacityDisplay,
+    interface_obj: state.interface,
+    slideOutStyles: state.video.videoLinkSlideOut,
+    toggleSlideOut: state.video.toggleSlideOut,
+    url: state.url
+  };
 }
 
 function validate(values) {
-    const error = {};
-    const youtubeLinkValue = values["youtube-url"];
-    console.log("VALUES: ", values);
-    if (youtubeLinkValue) {
-        if (youtubeLinkValue.indexOf("youtu") === -1) {
-            error["youtube-url"] = 'Please paste a valid YouTube URL'
-        }
+  const error = {};
+  const youtubeLinkValue = values['youtube-url'];
+  if (youtubeLinkValue) {
+    if (youtubeLinkValue.indexOf('youtu') === -1) {
+      error['youtube-url'] = 'Please paste a valid YouTube URL';
+    }
+  }
+  return error;
 }
-    return error;
-}
-
 
 VideoContainer = reduxForm({
-    form: 'youtube-url',
-    validate
-})(VideoContainer)
+  form: 'youtube-url',
+  validate
+})(VideoContainer);
 
-export default connect(mapStateToProps, { ...actions })(VideoContainer)
+export default connect(mapStateToProps, { ...actions })(VideoContainer);
--- client/src/reducers/videoReducer.js
+++ client/src/reducers/videoReducer.js
@@ -1,60 +1,75 @@
 import types from '../actions/types';
 
 const DEFAULT_STATE = {
-    results: [],
-    videoList: [],
-    videoTitle: '',
-    toggleResults: true,
-    addVideoModal: {
-        display: 'none'
-    },
-    resultsStyles: {
-        transform: 'translateX(100%)'
-    },
-    opacityDisplay: {
-        display: 'none'
-    },
-    videoLink: '',
-    videoLinkSlideOut: {
-        transform: 'translateY(-119px)'
-    },
-    toggleSlideOut: true
+  results: [],
+  videoList: [],
+  videoTitle: '',
+  toggleResults: true,
+  addVideoModal: {
+    display: 'none'
+  },
+  resultsStyles: {
+    transform: 'translateX(100%)'
+  },
+  opacityDisplay: {
+    display: 'none'
+  },
+  videoLink: '',
+  videoLinkSlideOut: {
+    transform: 'translateY(-119px)'
+  },
+  toggleSlideOut: true
 };
 
-export default function (state = DEFAULT_STATE, action) {
-    switch (action.type) {
-        case types.GET_VIDEO_RESULTS:
-            return { ...state, results: action.payload }
-        case types.ADD_TO_PLAYLIST:
-            return { ...state, videoList: [action.payload, ...state.videoList] };
-        case types.GET_RESULT_STYLES:
-            return { ...state, resultsStyles: action.payload, toggleResults: !state.toggleResults }
-        case types.GRAB_VIDEO_URL:
-            return { ...state, videoLink: action.payload }
-        case types.TOGGLE_RESULTS:
-            return { ...state, toggleResults: !state.toggleResults }
-        case types.GET_OPACITY_DISPLAY:
-            return { ...state, opacityDisplay: action.payload }
-        case types.TOGGLE_MODAL:
-            return { ...state, addVideoModal: { display: action.payload } }
-        case types.GET_VIDEO_TITLE:
-            if (!action.payload) {
-                return state;
-            }
-            return { ...state, videoTitle: action.payload }
-        case types.PLAY_PASTED_VIDEO_LINK:
-            return { ...state, videoLink: action.payload }
-        case types.PLAY_VIDEO:
-            return { ...state, videoLink: action.payload.videoId, resultsStyles: { transform: 'translateX(100%)' } }
-        case types.NO_VIDEO_LINK:
-            return { ...state }
-        case types.SET_VIDEO_URL:
-            return { ...state, videoLink: action.payload }
-        case types.TOGGLE_VIDEO_SLIDE_OUT:
-            return { ...state, videoLinkSlideOut: action.payload.slideOutStyles, toggleSlideOut: action.payload.toggleSlideOut }
-        case types.EMPTY_VIDEO_SLIDE_OUT:
-            return { ...state, videoLinkSlideOut: action.payload.slideOutStyles.style }
-        default:
-            return state;
-    }
+export default function(state = DEFAULT_STATE, action) {
+  switch (action.type) {
+    case types.GET_VIDEO_RESULTS:
+      return { ...state, results: action.payload };
+    case types.ADD_TO_PLAYLIST:
+      return { ...state, videoList: [action.payload, ...state.videoList] };
+    case types.GET_RESULT_STYLES:
+      return {
+        ...state,
+        resultsStyles: action.payload,
+        toggleResults: !state.toggleResults
+      };
+    case types.GRAB_VIDEO_URL:
+      return { ...state, videoLink: action.payload };
+    case types.TOGGLE_RESULTS:
+      return { ...state, toggleResults: !state.toggleResults };
+    case types.GET_OPACITY_DISPLAY:
+      return { ...state, opacityDisplay: action.payload };
+    case types.TOGGLE_MODAL:
+      return { ...state, addVideoModal: { display: action.payload } };
+    case types.GET_VIDEO_TITLE:
+      if (!action.payload) {
+        return state;
+      }
+      return { ...state, videoTitle: action.payload };
+    case types.PLAY_PASTED_VIDEO_LINK:
+      return { ...state, videoLink: action.payload };
+    case types.PLAY_VIDEO:
+      return {
+        ...state,
+        videoLink: action.payload.videoId,
+        resultsStyles: { transform: 'translateX(100%)' }
+      };
+    case types.NO_VIDEO_LINK:
+      return { ...state };
+    case types.SET_VIDEO_URL:
+      return { ...state, videoLink: action.payload };
+    case types.TOGGLE_VIDEO_SLIDE_OUT:
+      return {
+        ...state,
+        videoLinkSlideOut: action.payload.slideOutStyles,
+        toggleSlideOut: action.payload.toggleSlideOut
+      };
+    case types.EMPTY_VIDEO_SLIDE_OUT:
+      return {
+        ...state,
+        videoLinkSlideOut: action.payload.slideOutStyles.style
+      };
+    default:
+      return state;
+  }
 }
