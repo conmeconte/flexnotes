c768a8916126a093427e236a02b35685b53a6ff4
brucean52 <bruce.an52@gmail.com>
2018-02-26 13:02:58 -0800

    sample login working

--- client/src/reducers/binderArrayReducer.js
+++ client/src/reducers/binderArrayReducer.js
@@ -7,9 +7,9 @@ const DEFAULT_STATE = {
 export default function(state = DEFAULT_STATE, action){
     switch(action.type){
         case types.GET_USER_DATA:
-        return{ binderArr: [...action.payload.binder_arr_obj]}
+            return{ binderArr: [...action.payload.binder_arr_obj]}
         case types.FETCH_SAMPLE_USER:
-        return{ binderArr: [...action.payload.binder_arr_obj]}
+            return{ binderArr: [...action.payload.binder_arr_obj]}
         case types.UPDATE_BINDER_ARRAY:
             return{ binderArr: [...action.payload]}
         case types.ADD_BINDER:
