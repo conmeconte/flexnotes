ed19d1c6e18d6455b92ec9f52919292e57f1e2d4
John Hong <john@arcules.com>
2019-01-26 16:54:54 -0800

    add coverted privacy policy

---
+++ client/src/components/Privacy.js
@@ -0,0 +1,80 @@
+import React from 'react';
+
+export default function Privacy(){
+  return(
+    <>
+      <p className="c4 title" id="h.o57rgzsimwia"><span className="c10">FlexNotes Privacy Policy</span></p>
+      <p className="c0 c2"><span className="c1"></span></p>
+      <h1 className="c7" id="h.fznxgpf623a1"><span className="c9">Privacy Policy</span></h1>
+      <p className="c0 c2"><span className="c1"></span></p>
+      <p className="c0"><span>Last updated: </span><span className="c1 c6">01/03/2019</span></p>
+      <p className="c0 c2"><span className="c1"></span></p>
+      <p className="c0"><span className="c6">My Company FlexNotes</span><span>&nbsp;(&quot;us&quot;, &quot;we&quot;, or
+          &quot;our&quot;) operates </span><span className="c6">https://www.flexnotes.conmeconte.com</span><span className="c1">&nbsp;(the
+          &quot;Site&quot;). This page informs you of our policies regarding the collection, use and disclosure of Personal
+          Information we receive from users of the Site.</span></p>
+      <p className="c0 c2"><span className="c1"></span></p>
+      <p className="c0"><span className="c1">We use your Personal Information only for providing and improving the Site. By using
+          the Site, you agree to the collection and use of information in accordance with this policy.</span></p>
+      <p className="c0 c2"><span className="c1"></span></p>
+      <p className="c0"><span className="c3">Information Collection And Use</span></p>
+      <p className="c0 c2"><span className="c1"></span></p>
+      <p className="c0"><span className="c1">While using our Site, we may ask you to provide us with certain personally
+          identifiable information that can be used to contact or identify you. Personally identifiable information may
+          include, but is not limited to your name (&quot;Personal Information&quot;).</span></p>
+      <p className="c0 c2"><span className="c1"></span></p>
+      <p className="c0"><span className="c3">Log Data</span></p>
+      <p className="c0 c2"><span className="c1"></span></p>
+      <p className="c0"><span className="c1">Like many site operators, we collect information that your browser sends whenever you
+          visit our Site (&quot;Log Data&quot;).</span></p>
+      <p className="c0 c2"><span className="c1"></span></p>
+      <p className="c0"><span className="c1">This Log Data may include information such as your computer&#39;s Internet Protocol
+          (&quot;IP&quot;) address, browser type, browser version, the pages of our Site that you visit, the time and date
+          of your visit, the time spent on those pages and other statistics.</span></p>
+      <p className="c0 c2"><span className="c1"></span></p>
+      <p className="c0"><span className="c1">In addition, we may use third party services such as Google Analytics that collect,
+          monitor and analyze this &hellip;</span></p>
+      <p className="c0 c2"><span className="c1"></span></p>
+      <p className="c0 c2"><span className="c3"></span></p>
+      <p className="c0 c2"><span className="c3"></span></p>
+      <p className="c0"><span className="c3">Communications</span></p>
+      <p className="c0 c2"><span className="c1"></span></p>
+      <p className="c0"><span className="c1">We may use your Personal Information to contact you with newsletters, marketing or
+          promotional materials and other information that ...</span></p>
+      <p className="c0 c2"><span className="c1"></span></p>
+      <p className="c0 c2"><span className="c1"></span></p>
+      <p className="c0"><span className="c3">Cookies</span></p>
+      <p className="c0 c2"><span className="c1"></span></p>
+      <p className="c0"><span className="c1">Cookies are files with small amount of data, which may include an anonymous unique
+          identifier. Cookies are sent to your browser from a web site and stored on your computer&#39;s hard drive.</span></p>
+      <p className="c0 c2"><span className="c1"></span></p>
+      <p className="c0"><span className="c1">Like many sites, we use &quot;cookies&quot; to collect information. You can instruct
+          your browser to refuse all cookies or to indicate when a cookie is being sent. However, if you do not accept
+          cookies, you may not be able to use some portions of our Site.</span></p>
+      <p className="c0 c2"><span className="c1"></span></p>
+      <p className="c0"><span className="c3">Security</span></p>
+      <p className="c0 c2"><span className="c1"></span></p>
+      <p className="c0"><span className="c1">The security of your Personal Information is important to us, but remember that no
+          method of transmission over the Internet, or method of electronic storage, is 100% secure. While we strive to use
+          commercially acceptable means to protect your Personal Information, we cannot guarantee its absolute security.</span></p>
+      <p className="c0 c2"><span className="c1"></span></p>
+      <p className="c0"><span className="c5">Changes To This Privacy Policy</span></p>
+      <p className="c0 c2"><span className="c1"></span></p>
+      <p className="c0"><span>This Privacy Policy is effective as of </span><span className="c6">01/03/2019</span><span className="c1">&nbsp;and
+          will remain in effect except with respect to any changes in its provisions in the future, which will be in effect
+          immediately after being posted on this page.</span></p>
+      <p className="c0 c2"><span className="c1"></span></p>
+      <p className="c0"><span className="c1">We reserve the right to update or change our Privacy Policy at any time and you should
+          check this Privacy Policy periodically. Your continued use of the Service after we post any modifications to the
+          Privacy Policy on this page will constitute your acknowledgment of the modifications and your consent to abide
+          and be bound by the modified Privacy Policy.</span></p>
+      <p className="c0 c2"><span className="c1"></span></p>
+      <p className="c0"><span className="c1">If we make any material changes to this Privacy Policy, we will notify you either
+          through the email address you have provided us, or by placing a prominent notice on our website.</span></p>
+      <p className="c0 c2"><span className="c1"></span></p>
+      <p className="c0"><span className="c3">Contact Us</span></p>
+      <p className="c0 c2"><span className="c1"></span></p>
+      <p className="c0"><span className="c1">If you have any questions about this Privacy Policy, please contact us.</span></p>
+    </>
+  )
+}
